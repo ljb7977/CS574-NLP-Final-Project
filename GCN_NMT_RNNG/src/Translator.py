bf6240ff11d71ef176c741221d921a98e16c5b98
rokrokss <q0115643@kaist.ac.kr>
2018-06-10 06:27:09 +0900

    0

--- GCN_NMT_RNNG/src/Translator.py
+++ GCN_NMT_RNNG/src/Translator.py
@@ -333,7 +333,6 @@ class Translator(object):
              hiddenEncDim,
              hiddenActDim,
              scale,
-             beamSize,
              miniBatchSize,
              learningRate,
              loadModel,
